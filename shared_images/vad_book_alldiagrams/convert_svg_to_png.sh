ea0c47bb05d52857158272138a48d9d367a0afef
John Alexis Guerra Gómez <john.guerra@gmail.com>
2020-07-29 09:52:31 -0500

    Templates for everything else

--- shared_images/vad_book_alldiagrams/convert_svg_to_png.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-for FILE in `ls *.pdf`
-do
-  echo convert -density 150 -depth 8 -quality 85 "$FILE" "${FILE%.pdf}.png"
-  convert -density 150 -depth 8 -quality 85 "$FILE" "${FILE%.pdf}.png"
-done
